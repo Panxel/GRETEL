af387707b4649353abf57c5202e67409c38c7871
Daniel <daniel.fu94360@gmail.com>
2024-01-09 09:57:55 +0100

    add test from prof and solved a bit generator

--- src/dataset/generators/twitter.py
+++ src/dataset/generators/twitter.py
@@ -50,7 +50,7 @@ class TwitterGCN(Generator):
         # print(graph_nodes)
         adj_matrix = []
         for key in graph_nodes.keys():
-            if key >= self.num_instances:
+            if key > self.num_instances:
                 break
             adj_matrix.append(np.zeros((len(graph_nodes[key]), len(graph_nodes[key]))))
             # print("HERE",index)
@@ -71,20 +71,20 @@ class TwitterGCN(Generator):
         tuple_list = [tuple(map(int, pair.split(','))) for pair in lines]
 
         # Using a list comprehension to find the key(s) for the given value
-        print("HERE1")
+        # print("HERE1")
         for tuple_item in tuple_list:
             desired_value = tuple_item[0]
             keys_for_value = [key for key, value in graph_nodes.items() if desired_value in value]
-            if keys_for_value[0] >= self.num_instances:
+            if keys_for_value[0] > self.num_instances:
                 break
             current_graph_id_list = graph_nodes[keys_for_value[0]]
             adj_matrix[keys_for_value[0]-1][current_graph_id_list.index(tuple_item[0])][current_graph_id_list.index(tuple_item[1])] = 1
             # print("NEXT")
             # print(adj_matrix[keys_for_value[0]-1])
-            print(keys_for_value[0])
+            # print(keys_for_value[0])
             
             
-        print("HERE2")
+        # print("HERE2")
 
         # Open the file and read the lines
         with open(self._graph_labels_file_path, 'r') as file:
@@ -94,8 +94,14 @@ class TwitterGCN(Generator):
         label_list = [line.strip() for line in lines]
 
         # print(lines)
-        print("HERE3")
+        # print("HERE3")
+
+
+
         for key in graph_nodes.keys():
-            if key >= self.num_instances:
+            if key > self.num_instances:
                 break
-            self.dataset.instances.append(GraphInstance(id = int(key) , label = int(label_list[key-1]), data = adj_matrix[key-1]))
+            label = 1
+            if int(label_list[key-1]) == -1 :
+                label = 0
+            self.dataset.instances.append(GraphInstance(id = int(key) , label = label, data = adj_matrix[key-1]))
